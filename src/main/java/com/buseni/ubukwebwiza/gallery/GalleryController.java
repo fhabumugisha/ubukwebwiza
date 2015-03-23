06dc58fa4e331c5cb71fe7d9882cd1d068dda934
fhabumugisha <fabriceboris@gmail.com>
2015-03-23 22:34:44 +0100

    update sowimage

--- src/main/java/com/buseni/ubukwebwiza/gallery/GalleryController.java
+++ src/main/java/com/buseni/ubukwebwiza/gallery/GalleryController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -48,7 +49,10 @@ public class GalleryController {
 		Photo photo = photoService.findById(imageId);
 		byte[] imageContent = photo.getContent();
 		response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+		response.setContentLength(imageContent.length);
+		response.setStatus(HttpStatus.OK.value());
 		response.getOutputStream().write(imageContent);
+		response.getOutputStream().flush();
 		response.getOutputStream().close();
 	}
 	@ModelAttribute("currentMenu")
