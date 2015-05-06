3f666b624b267cc07eec8d2180e55c7fb7f2fbf4
fhabumugisha <fabriceboris@gmail.com>
2015-05-07 00:52:30 +0200

    Update the select html tag with Chosen Jquery plugin

--- src/main/java/com/buseni/ubukwebwiza/utils/AmazonS3Util.java
+++ src/main/java/com/buseni/ubukwebwiza/utils/AmazonS3Util.java
@@ -6,9 +6,7 @@ import java.io.InputStream;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.AmazonClientException;
