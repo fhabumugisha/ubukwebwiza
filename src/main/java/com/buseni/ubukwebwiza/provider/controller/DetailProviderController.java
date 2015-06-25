db7ce5e6ee6c8fcafe1b760c7e63dbe88148b137
fhabumugisha <fabriceboris@gmail.com>
2015-06-25 10:34:32 +0200

    Refactor the code

--- src/main/java/com/buseni/ubukwebwiza/provider/controller/DetailProviderController.java
+++ src/main/java/com/buseni/ubukwebwiza/provider/controller/DetailProviderController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.buseni.ubukwebwiza.breadcrumbs.navigation.Navigation;
 import com.buseni.ubukwebwiza.home.HomeController;
@@ -18,26 +17,18 @@ import com.buseni.ubukwebwiza.provider.service.ProviderService;
 
 @Controller
 @Navigation(url="/providers/{id:[\\d]+}", name="Provider", parent={ ListProviderController.class, HomeController.class})
-@SessionAttributes({"allWeddingServices","allDistricts"})
 public class DetailProviderController {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger( DetailProviderController.class );
 
-	
-
 	@Autowired
 	private ProviderService providerService;
-
-
-
 	@RequestMapping(value="/providers/{id:[\\d]+}",method=RequestMethod.GET)
-	public String getProvider(@PathVariable Integer id, Model model){
+	public String getProvider(@PathVariable Integer id, Model model){		
 		Provider provider = providerService.getProvider(id);
-		model.addAttribute("provider", provider);
+		model.addAttribute("provider", provider);		
 		return "frontend/provider/detailProvider";
 	}
-
-	
 
 	@ModelAttribute("currentMenu")
 	public String module(){
